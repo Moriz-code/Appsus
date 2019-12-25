f8e086b43069a80072e546e41524679972e7488f
moriz-code <morzavi1@gmail.com>
2019-12-25 11:06:29 +0200

    hi

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,7 @@ export default class Home extends React.Component {
     render() {
         return (
             <section>
-                <h1>HOME sweet home</h1>
+                <h1>HOME v;d;gjkrlegje sweet home</h1>
 
                 <p>we are dream tea.m!!!!</p>
             </section>
